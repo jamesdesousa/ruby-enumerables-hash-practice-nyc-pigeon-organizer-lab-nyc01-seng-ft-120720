1f3f4c4eb8cc0e740aace105e592fbd03fa579d0
James DeSousa <james.desousa45@gmail.com>
2020-11-28 00:17:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,8 +10,8 @@ def nyc_pigeon_organizer(data)
             final_array[name][key] = []
         end
         #binding.pry 
-        inner_key.to_s 
-        final_array[name][key].push(inner_key) 
+        
+        final_array[name][key].push(inner_key.to_s) 
       end 
     end 
    end 
